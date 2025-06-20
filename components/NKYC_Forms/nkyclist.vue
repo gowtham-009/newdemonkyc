718818e96456dd733ceac25348f7e60dbef51954
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-06-20 16:25:20 +0530

    hui

--- components/NKYC_Forms/nkyclist.vue
+++ components/NKYC_Forms/nkyclist.vue
@@ -171,9 +171,10 @@ const handleButtonClick = () => {
         }
         else {
         const digiInfo = data?.payload?.metaData?.digi_info || [];
+        const digiadd = data?.payload?.metaData?.digi_info.aadhaarUID
         const panInfo = data?.payload?.metaData?.kraPan?.APP_KRA_INFO;
 
-        if (digiInfo.length === 0) {
+        if (digiInfo.length === 0 && digiadd) {
           if (panInfo) {
             pagestatus('parmanentaddress');
             emit('updateDiv', 'parmanentaddress');
